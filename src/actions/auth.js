8d35a15d5a3c81a42348b6b38ca41b7280154663
Jennifer <jlawrence010@gmail.com>
2018-07-30 22:26:22 -0700

    finished scaffolding, made edits

---
+++ src/actions/auth.js
@@ -0,0 +1,34 @@
+import superagent from 'superagent';
+import * as routes from '../lib/routes';
+
+// These are sync action creators
+
+export const setToken = token => ({
+  type: 'TOKEN_SET',
+  payload: token,
+});
+
+export const removeToken = () => ({
+  type: 'TOKEN_REMOVE',
+});
+
+// These are async action creators
+
+export const userSignup = user => (store) => {
+  return superagent.post(`${API_URL}${routes.SIGNUP_ROUTE}`)
+    .send(user)
+    .withCredentials() 
+    .then((response) => {
+      return store.dispatch(setToken(response.body.token));
+    });
+};
+
+// userLogin(user)(store)
+export const userLogin = user => (store) => {
+  return superagent.get(`${API_URL}${routes.LOGIN_ROUTE}`)
+    .auth(user.username, user.password)
+    .withCredentials()
+    .then((response) => {
+      return store.dispatch(setToken(response.body.token));
+    });
+};
